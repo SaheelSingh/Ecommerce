20f65889cccc305ee7548877cbd0dbe4117010cd
SaheelSingh <saheelsingh1904@gmail.com>
2024-08-30 17:26:36 +0530

    Dockerfile added in both client and api

--- api/db.js
+++ api/db.js
@@ -1,16 +1,32 @@
 const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-dotenv.config();
-const connectDb = () => {
+let db = null;
+const option = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 5000,
+    autoIndex: false,
+    maxPoolSize: 10,
+    serverSelectionTimeoutMS: 5000,
+    socketTimeoutMS: 45000, 
+    family: 4,
+    directConnection:true
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+const connectDb = async () => {
     try {
-        mongoose.connect(process.env.MONGO_URL, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        })
+        db = await mongoose.connect('mongodb://root:secret@db:27017', option)
         console.log('connected to Database');
     }
     catch(err) {
-        console.log('failed to connect')
+        if(!db) {
+            await sleep(5000)
+            connectDb()
+        }
+        console.log(err)
     }
 }
 
